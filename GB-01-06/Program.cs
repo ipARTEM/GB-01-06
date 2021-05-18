72026014e95fbb56610eca940bcf144188e4a78e
Artem <a.khimin@yandex.ru>
2021-05-18 15:55:47 +0300

    Вывод в консоль id и имя запущенный процессов

--- GB-01-06/Program.cs
+++ GB-01-06/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,37 @@ namespace GB_01_06
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("   ***Task Manager***   ");
+            #region   Test
+
+            ////  завершает процесс в экстренном режиме
+            //Process notepad = Process.Start("notepad.exe");
+            //Console.WriteLine("Нажмите любую клавишу для завершения процесса...");
+            //Console.ReadKey();
+            //notepad.Kill();
+            //Console.WriteLine(notepad.ExitCode); // -1
+
+            //// завершает процесса в штатном режиме
+            //Process notepad2 = Process.Start("notepad.exe");
+            //Console.WriteLine("Для продолжения, закройте окно Блокнота...");
+            //notepad2.WaitForExit();
+            //Console.WriteLine(notepad2.ExitCode); // 0
+
+            #endregion
+
+            Process[] processes = Process.GetProcesses();
+
+            Console.WriteLine("  Id процесса   | Имя процесса");
+            foreach (var i in processes)
+            {
+                Console.WriteLine($"  {i.Id}          {i.ProcessName}") ;
+
+            };
+
+
+
+
         }
     }
 }
